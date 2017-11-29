7334bfbf954f78fd964658d1963f884d6cdce425
Marcin <Marcin@github.pl>
2017-11-29 09:44:48 +0100

    po dodaniu godziny i daty

---
+++ app/src/main/java/com/example/marcin/aplikacja_inzynierska/DodajRezerwacjeGodzinaDataActivity.java
@@ -0,0 +1,13 @@
+package com.example.marcin.aplikacja_inzynierska;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DodajRezerwacjeGodzinaDataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dodaj_rezerwacje_godzina_data);
+    }
+}
--- app/src/main/java/com/example/marcin/aplikacja_inzynierska/MainActivity.java
+++ app/src/main/java/com/example/marcin/aplikacja_inzynierska/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.marcin.aplikacja_inzynierska;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // zmainy zmainy hhghf
+
+        Button dodajgodzinedate = (Button) findViewById(R.id.buttondodajrezerwacje);
+        dodajgodzinedate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), DodajRezerwacjeGodzinaDataActivity.class);
+                startActivity(i);
+
+            }
+        });
+
     }
 }
